ea8e803c212eafd2a7f8cf0d929088ca48b89965
Dani <danihazler@gmail.com>
2021-05-03 16:54:31 +0100

    Update homepage and fetch employee of the month

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
   return (  
-    <>helloooo</>
+    <div className='page-container'>
+      <main className={styles.main}>
+        <h1> Next.js News App</h1>
+        <h3>Your one stop shop for the latest news articles.</h3>
+      </main>
+    </div>
   )
 }
