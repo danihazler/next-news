10b7ec0ae25e5d100aeb1e3b76cdbcee05283997
Dani <danihazler@gmail.com>
2021-08-06 18:01:15 +0100

    Remove unnecessary css file

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -9,36 +9,36 @@ const Contact = () => {
                 </p>
 
                 <form name="contact" action="/success" method="POST" data-netlify="true">
-                <input type="hidden" name="form-name" value="contact"/>
-                <p>
-                    <label htmlFor="firstname">First Name: <input type="text" name="firstname" id="firstname" /></label>   
-                </p>
-                <p>
-                    <label htmlFor="lastname">Last Name: <input type="text" name="lastname" id="lastname"/></label>   
-                </p>
-                <p>
-                    <label htmlFor="email">Your Email: <input type="email" name="email" id="email" /></label>
-                </p>
-                <p>
-                    <label htmlFor="people">How many people: 
-                    <select name="people" id="people">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select>
-                    </label>
-                </p>
-                <p>
-                    <label htmlFor="termsandconditions">Agree with T&amp;C: <input required type="checkbox" name="termsandconditions" id="termsandconditions"/></label>
-                </p>
-                <p>
-                    <label htmlFor="oktobecontacted">Agree to be contacted: <input required type="checkbox" name="oktobecontacted" id="oktobecontacted"/></label>
-                </p>
-                <p>
-                    <button type="submit">Sign up</button>
-                </p>
+                    <input type="hidden" name="form-name" value="contact"/>
+                    <p>
+                        <label htmlFor="firstname">First Name: <input type="text" name="firstname" id="firstname" /></label>   
+                    </p>
+                    <p>
+                        <label htmlFor="lastname">Last Name: <input type="text" name="lastname" id="lastname"/></label>   
+                    </p>
+                    <p>
+                        <label htmlFor="email">Your Email: <input type="email" name="email" id="email" /></label>
+                    </p>
+                    <p>
+                        <label htmlFor="people">How many people: 
+                        <select name="people" id="people">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                        </label>
+                    </p>
+                    <p>
+                        <label htmlFor="termsandconditions">Agree with T&amp;C: <input required type="checkbox" name="termsandconditions" id="termsandconditions"/></label>
+                    </p>
+                    <p>
+                        <label htmlFor="oktobecontacted">Agree to be contacted: <input required type="checkbox" name="oktobecontacted" id="oktobecontacted"/></label>
+                    </p>
+                    <p>
+                        <button type="submit">Sign up</button>
+                    </p>
                 </form>
             </main>
         </div>
